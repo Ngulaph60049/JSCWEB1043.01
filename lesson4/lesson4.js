d780f0fa04bb21468da3094d76f48b724e20d922
ngu324 <anhngu29052006@gmail.com>
2026-03-17 17:56:26 +0700

    first commit

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,7 +1,15 @@
 //If else: la 1 cau truc dieu khien voi 2 block code cua if va else
-const age = 17
-if (age < 18) {
-    alert("Em chua 18")
-} else {
-    alert("Du tuoi")
-}
+// const age = 17
+// if (age < 18) {
+//     alert("Em chua 18")
+// } else {
+//     alert("Du tuoi")
+// }
+
+const tuoi = Number(prompt("Nhập vào độ tuổi của bạn:"));
+let doTuoi = (tuoi >= 80) ? "Người già" :
+             (tuoi >= 60) ? "Trung niên" :
+             (tuoi >= 40) ? "Trưởng thành" :
+             (tuoi >= 20) ? "Thanh niên" : 
+                            "Trẻ em";
+alert(doTuoi);
